909b946923759e172dbc81674e9e2c5fc1300ab4
Ricardo <57273461+RicardoLopez9908@users.noreply.github.com>
2023-05-01 20:48:26 -0300

    improve common components and albums screen (#12)

---
+++ app/src/main/java/com/example/photogallery/presentation/base/view/BaseScreen.kt
@@ -0,0 +1,69 @@
+package com.example.photogallery.presentation.base.view
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridScope
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import com.example.photogallery.R
+import com.example.photogallery.util.Response
+
+@Composable
+fun BaseScreenStateHandler(
+    response: Response<Any?>,
+    onClickRetry: () -> Unit,
+    successContent: @Composable () -> Unit
+) {
+    when (response) {
+        is Response.Success -> successContent()
+        is Response.Loading -> LoadingScreen()
+        is Response.Error -> ErrorScreen(
+            e = response.exception,
+            retryOnClick = onClickRetry
+        )
+    }
+}
+
+/** This screen use LazyVerticalStaggeredGrid with Fixed(2) and DefaultToolbar */
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun DefaultScrollableScreenWithToolbar(
+    title: String? = null,
+    changeThemeColor: () -> Unit,
+    onClickRetry: () -> Unit,
+    content: LazyStaggeredGridScope.() -> Unit
+) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            DefaultToolbar(
+                scrollBehavior = scrollBehavior,
+                title = title,
+                menuContent = {
+                    ChangeThemeButton(changeThemeColor)
+                    RefreshButton(onClickRetry)
+                }
+            )
+        }
+    ) { paddingValues ->
+        LazyVerticalStaggeredGrid(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(horizontal = dimensionResource(id = R.dimen.base_horizontal_padding)),
+            columns = StaggeredGridCells.Fixed(2),
+            verticalItemSpacing = dimensionResource(id = R.dimen.base_item_spacing_padding),
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.base_item_spacing_padding)),
+            content = content
+        )
+    }
+}

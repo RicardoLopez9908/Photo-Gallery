995bd2c78143453c2aeda0e63ffb0f24881a58ae
Ricardo <57273461+RicardoLopez9908@users.noreply.github.com>
2023-05-03 09:56:00 -0300

    improve viewModels scope (#21)

--- app/src/main/java/com/example/photogallery/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/presentation/main/MainActivity.kt
@@ -7,26 +7,26 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.photogallery.presentation.albums.viewmodel.AlbumsViewModel
 import com.example.photogallery.presentation.main.navigation.SetupNavGraph
-import com.example.photogallery.presentation.main.viewmodel.ThemeViewModel
 import com.example.photogallery.ui.theme.PhotoGalleryTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val themeViewModel: ThemeViewModel by viewModels()
+    private val albumsViewModel: AlbumsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val darkMode = themeViewModel.isDarkTheme.collectAsState()
+            val darkMode = albumsViewModel.isDarkTheme.collectAsState()
             PhotoGalleryTheme(
                 darkTheme = darkMode.value ?: isSystemInDarkTheme()
             ) {
                 SetupNavGraph(
                     navController = rememberNavController(),
-                    changeThemeColor = themeViewModel::saveIsDarkThemeValue
+                    albumsViewModel = albumsViewModel
                 )
             }
         }
